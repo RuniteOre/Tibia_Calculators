f672291d40bd0284d27154314c0d13a79fa7be7f
RuniteOre <marksteffler@Marks-MacBook-Air.local>
2023-03-23 22:08:41 -0400

    Added in a function for expected magic damage, minor fixes.

--- tibia_max_dam_calc.py
+++ tibia_max_dam_calc.py
@@ -1,5 +1,7 @@
 import math
+import json
 
+#different functions for the correct Formulae for Offensive, Balanced, and Defensive Stances
 def offensive_mode(base_damage, weapon_damage, weapon_skill):
 	global offensive
 	offensive = base_damage + math.floor(math.floor(1.2*weapon_damage)*((weapon_skill+4)/28))
@@ -12,7 +14,30 @@ def defensive_mode(base_damage, weapon_damage, weapon_skill):
 	global defensive
 	defensive = base_damage + math.floor(math.ceil(0.6 * weapon_damage) * (weapon_skill + 4) / 28)
 
+def howthefuckdoesmagicwork(level, base_damage, weapon_damage_min, weapon_damage_max, magic_level, spell_name):
+	global magic
+	with open("instantspells.json", "r") as f:
+		spell_data = json.load(f)[spell_name]
+
+	spell_x = int(float(spell_data["Max damage"]["x"]) - float(spell_data["Min damage"]["x"]))
+	spell_y = int(spell_data["Max damage"]["y"]) - float(spell_data["Min damage"]["y"])
+
+	x = int(float(spell_data["Min damage"]["x"]) + spell_x * int(magic_level))
+	y = int(float(spell_data["Min damage"]["y"]) + spell_y * int(magic_level))
+
+	magic = math.floor((level * 0.2) + x + y)
+
+	return magic
+
+#calculating "s" for the base damage formula
 def calculate_s(level):
+#	Till level 500: no change, damage and healing +1 every 5 levels
+#	As of level 501-1100: damage and healing +1 every 6 levels
+#	As of level 1101-1800: damage and healing +1 every 7 levels
+#	As of level 1801-2600: damage and healing +1 every 8 levels
+#	As of level 2601-3500: damage and healing +1 every 9 levels
+#	Above level 3500 it continues with the same logic (steps of 500, 600, 700, etc.)
+#the if statements are used to calculate the correct "s" value for the base damage formula from the above info
     if level <= 500:
         return 5
     elif level <= 1100:
@@ -26,27 +51,35 @@ def calculate_s(level):
     else:
         return math.floor((math.sqrt(2 * level + 2025) + 5 / 10))
 
+#calculating "b" for the base damage formula
 def calculate_b(level, s):
     return math.floor((level + 1000) / s - (50 * s)) + (100 * s) - 450
 
+#main menu
 def main_menu():
     print("#" * 40)
     print("#" + " " * 38 + "#")
     print("#" + " " * 9 + "Mark's Tibia Calculators" + " " * 5 + "#")
     print("#" + " " * 38 + "#")
     print("#" * 40)
+    print("#" + " " * 38 + "#")
     print("#" + " " * 3 + "1. Base Damage & Healing" + " " * 11 + "#")
-    print("#" + " " * 3 + "2. Max Damage & Healing" + " " * 12 + "#")
-    print("#" + " " * 3 + "3. Mana Training Time" + " " * 14 + "#")
-    print("#" + " " * 3 + "4. Magic Power" + " " * 21 + "#")
-    print("#" + " " * 3 + "5. Speed" + " " * 27 + "#")
+    print("#" + " " * 3 + "2. Melee Max Damage" + " " * 16 + "#")
+    print("#" + " " * 3 + "3. Magic Max Damage" + " " * 16 + "#")
+    print("#" + " " * 3 + "4. Mana Training Time" + " " * 14 + "#")
+    print("#" + " " * 3 + "5. Magic Power" + " " * 21 + "#")
+    print("#" + " " * 3 + "6. Speed" + " " * 27 + "#")
+    print("#" + " " * 38 + "#")
     print("#" * 40)
     choice = input("Enter your choice: ")
     if choice == "1":
         base_damage_menu()
     if choice == "2":
-    	expected_damage_menu()
+    	expected_melee_damage_menu()
+    if choice == "3":
+    	expected_magic_damage_menu()
 
+#base damage menu
 def base_damage_menu():
 	print("#" * 40)
 	print("#" + " " * 38 + "#")
@@ -60,27 +93,45 @@ def base_damage_menu():
 	print(f"\nPlayer Level: {level}")
 	print(f"Base Damage & Healing: {base_damage}")
 
-def expected_damage_menu():
+#expected damage menu
+def expected_melee_damage_menu():
 	print ("#" * 40)
 	print ("#" + " " * 38 + "#")
 	print ("#" + " " * 5 + "Tibia Expected Melee Damage" + " " * 6 + "#")
 	print ("#" + " " * 11 + "Weapon Stances:" + " " * 12 + "#")
 	print ("#" + " " * 4 + "Offensive, Balanced, Defensive" + " " * 4 + "#")
+	print("#" + " " * 38 + "#")
 	print ("#" * 40)
 	base_damage = int(input("Enter base damage: "))
 	weapon_damage = int(input("Enter weapon damage: "))
-	weapon_skill = int(input("Enter weapon skill: "))
-	weapon_stance = (input("Enter weapon stance: "))
-	if weapon_stance == "offensive":
-		offensive_mode(base_damage, weapon_damage, weapon_skill)
-		print(f"\nExpected Damage: {offensive}")
-	if weapon_stance == "balanced":
-		balanced_mode(base_damage, weapon_damage, weapon_skill)
-		print(f"\nExpected Damage: {balanced}")
-	if weapon_stance == "defensive":
-		defensive_mode(base_damage, weapon_damage, weapon_skill)
-		print(f"\nExpected Damage: {defensive}")
+	weapon_skill = int(input("Enter weapon skill level: "))
+	offensive_mode(base_damage, weapon_damage, weapon_skill)
+	print(f"\nExpected Damage using Offensive: {offensive}")
+	balanced_mode(base_damage, weapon_damage, weapon_skill)
+	print(f"\nExpected Damage using Balanced: {balanced}")
+	defensive_mode(base_damage, weapon_damage, weapon_skill)
+	print(f"\nExpected Damage using Defensive: {defensive}")
+
+#expected magic damage menu
+def expected_magic_damage_menu():
+	print ("#" * 40)
+	print ("#" + " " * 38 + "#")
+	print ("#" + " " * 5 + "Tibia Expected Magic Damage" + " " * 6 + "#")
+	print ("#" + " " * 38 + "#")
+	print ("#" * 40)
+	level = int(input("Enter player level: "))
+	s = calculate_s(level)
+	b = calculate_b(level, s)
+	base_damage = b + (s - 1) * math.ceil(b / 2)
+	print(f"Base Damage: {base_damage}")
+	weapon_damage_min = int(input("Enter minimum weapon damage: "))
+	weapon_damage_max = int(input("Enter maximum weapon damage: "))
+	magic_level = str(input("Enter magic level: "))
+	spell_name = input("Enter spell name: ")
+	howthefuckdoesmagicwork(level, base_damage, weapon_damage_min, weapon_damage_max, magic_level, spell_name)
+	print(f"\nExpected Damage using {spell_name}: {magic}")
 
 
+#main function call
 if __name__ == "__main__":
 	main_menu()
