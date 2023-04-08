9027bc3da973f12e67a4f84afef5e45f0548c975
RuniteOre <marksteffler@Marks-MacBook-Air.local>
2023-04-07 23:15:11 -0400

    removed GNU header from license change

--- tibia_calcs.py
+++ tibia_calcs.py
@@ -1,24 +1,3 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-
-# TibiaCalcs: A collection of calculators for the MMORPG Tibia
-# Copyright (C) 2023 Mark Steffler
-# This program is free software: you can redistribute it and/or modify
-# it under the  terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# To learn more, read the documentation:
-#     https://github.com/RuniteOre/Tibia_Calculators
-
 import math
 import json
 
@@ -205,8 +184,8 @@ def magic_level_training_menu():
 	print ("#" * 40)
 
 	while True:
-		vocation = input("Enter vocation (Sorcerer, Master Sorcerer, Knight, Elite Knight, Paladin, Royal Paladin): ")
-		if vocation in ["Sorcerer", "Master Sorcerer", "Knight", "Elite Knight", "Paladin", "Royal Paladin"]:
+		vocation = input("Enter vocation (Sorcerer, Master Sorcerer, Knight, Elite Knight, Paladin, Royal Paladin, Druid, Elder Druid): ")
+		if vocation in ["Sorcerer", "Master Sorcerer", "Knight", "Elite Knight", "Paladin", "Royal Paladin", "Druid", "Elder Druid"]:
 			break
 		else:
 			print("Invalid vocation. Please enter a valid vocation")
